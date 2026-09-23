6e4d4891745390e2a18a07752a2eb3ed1d662909
dongducCheri <dongxuanduc2205@gmail.com>
2026-09-24 01:43:12 +0700

    func: category

--- minishop/config/routes.rb
+++ minishop/config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
+      #authenticate routes
       post '/login', to: 'sessions#create'
       post '/logout', to: 'sessions#destroy'
       get '/me', to: 'sessions#show'
+
+      #category routes
+      get    '/categories',     to: 'categories#index'   
+      get    '/categories/:id', to: 'categories#show'    
+      post   '/categories',     to: 'categories#create' 
+      patch  '/categories/:id', to: 'categories#update'  
+      delete '/categories/:id', to: 'categories#destroy' 
     end
   end
 end
